a0b9049c18f78aad785052b9178d3b5933e2fac5
78LUKMAN <lh6408421@gmail.com>
2023-12-01 20:26:36 +0700

    update: send products data to main page

--- Aplikasi/toko-online-produk-pertanian/app/Controllers/Home.php
+++ Aplikasi/toko-online-produk-pertanian/app/Controllers/Home.php
@@ -1,12 +1,22 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ProductModel;
 
 class Home extends BaseController
 {
+    function __construct()
+    {
+        helper('number');
+        helper('form');
+    }
+    
     public function index()
     {
+        $productModel = new ProductModel();
+        $products = $productModel->findAll();
+        $data['products'] = $products;
+        
         return view('pages/main/main');
     }
 }
- 
