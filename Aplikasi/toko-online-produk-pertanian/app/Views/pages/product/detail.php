01fabab05888fa498c388092ae95ed1bfced7fc2
78LUKMAN <lh6408421@gmail.com>
2024-01-08 11:43:38 +0700

    update

--- Aplikasi/toko-online-produk-pertanian/app/Views/pages/product/detail.php
+++ Aplikasi/toko-online-produk-pertanian/app/Views/pages/product/detail.php
@@ -21,14 +21,7 @@
 
     <div class="pagetitle">
         <h1>Product Detail</h1>
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item">Tables</li>
-                <li class="breadcrumb-item active">Data</li>
-            </ol>
-        </nav>
-    </div><!-- End Page Title -->
+    </div>
 
     <section class="section">
         <div class="row">
